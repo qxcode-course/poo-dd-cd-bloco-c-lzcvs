ffc95e631647ce2c3674286985d2eeba652839e5
luiza <luizasens1@gmail.com>
2025-11-13 15:05:51 -0300

    metodos arrive str completos. 40min

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -15,12 +15,24 @@ class Pessoa:
 
 class Market:
     def __init__(self, caixas: int):
-        self.__caixas: list[Pessoa | None] = []
-        self.__espera: list[Pessoa] = []
-        self.__numerocaixas = caixas
+        self.caixas: list[Pessoa | None] = [None] * caixas
+        self.espera: list[Pessoa] = []
+        self.numerocaixas = caixas
+
+    def arrive(self, pessoa: Pessoa):
+        self.espera.append(pessoa)
+    
+    def call(self, index: int):
+        self.espera[index] = self.caixas[0]
+        #atualizar valor self.caixas
     def __str__(self) -> str:
-        
-        #return f'{self.__caixas}\nEspera: []'
+        nomes = "[" + ', '.join(self.espera)+ "]" #sequence item 0: expected str instance
+        caixas_str = list(map(lambda caixa: '-----' if caixa is None else caixa, self.caixas))
+        caixas_str2 = ', '.join(caixas_str)
+        resultado = f"Caixas: [{caixas_str2}]\nEspera: {nomes}"
+        return resultado
+
+
 def main():
     mercado = Market(0)
     while True:
@@ -33,5 +45,9 @@ def main():
             print(mercado)
         elif args[0] == 'init':
             mercado = Market(int(args[1]))
-            
+        elif args[0] == 'arrive':
+            mercado.arrive((args[1]))
+        elif args[0] == 'call':
+            index = int(args[1])
+            mercado.call(index)
 main()
