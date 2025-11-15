a0b5186e3b96fec3307587fe72ffced2a5936970
luiza <luizasens1@gmail.com>
2025-11-15 08:43:54 -0300

    budega completinha. 1h40min

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -11,26 +11,46 @@ class Pessoa:
         self.__nome = nome
     
     def __str__(self):
-        return f"nome: {self.__nome}"
+        return self.__nome
 
 class Market:
     def __init__(self, caixas: int):
-        self.caixas: list[Pessoa | None] = [None] * caixas
+        self.caixas: list[Pessoa | None] = [None]
+        for _ in range(caixas - 1):
+            self.caixas.append(None)
         self.espera: list[Pessoa] = []
         self.numerocaixas = caixas
 
     def arrive(self, pessoa: Pessoa):
         self.espera.append(pessoa)
     
-    def call(self, index: int):
-        self.espera[index] = self.caixas[0]
-        #atualizar valor self.caixas
+    def call(self, index: int) -> bool:
+        if self.caixas[index] != None:
+            print("fail: caixa ocupado")
+            return False
+        if len(self.espera) > 0:
+            self.caixas[index] = self.espera.pop(0)
+        else:
+            print('fail: sem clientes')
+
+    def finish(self, index: int) -> bool:
+        if index < 0 or index >= len(self.caixas):
+            print('fail: caixa inexistente')
+            return False
+        if self.caixas[index] is None:
+            print('fail: caixa vazio')
+            return False
+        self.caixas[index] = None
+        return True
+
     def __str__(self) -> str:
-        nomes = "[" + ', '.join(self.espera)+ "]" #sequence item 0: expected str instance
-        caixas_str = list(map(lambda caixa: '-----' if caixa is None else caixa, self.caixas))
+        nomes = ['' if espera is None else str(espera) for espera in self.espera]
+        nomes_str = ', '.join(nome for nome in nomes if nome)
+
+        caixas_str = ['-----' if caixa is None else str(caixa) for caixa in self.caixas]
         caixas_str2 = ', '.join(caixas_str)
-        resultado = f"Caixas: [{caixas_str2}]\nEspera: {nomes}"
-        return resultado
+
+        return f"Caixas: [{caixas_str2}]\nEspera: [{nomes_str}]"
 
 
 def main():
@@ -50,4 +70,7 @@ def main():
         elif args[0] == 'call':
             index = int(args[1])
             mercado.call(index)
+        elif args[0] == 'finish':
+            index = int(args[1])
+            mercado.finish(index)
 main()
